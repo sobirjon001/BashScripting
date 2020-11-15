38b13db8b71b7293c66fd781d3de09725ba17ab3
sobirjon001 <sobirjon001@gmail.com>
2020-11-15 17:14:04 -0500

    new

--- Dialog/tutorial.sh
+++ Dialog/tutorial.sh
@@ -1,13 +1,1 @@
 #! /bin/bash
-
-dat=$(dialog --stdout --title "My Calendar" \
-                   --calendar "Select a date:" 0 0 25 12 2009)
-
-case $? in
-0)
- echo "You have entered: $dat"   ;;
-1)
- echo "You have pressed Cancel"  ;;
-255)
- echo "Box closed"   ;;
-esac
